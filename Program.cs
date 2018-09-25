6706df24848a2951050399d020851dbb22c6cc2e
MosaKasem <thescorpion119@gmail.com>
2018-09-25 13:11:13 +0200

    first commit - dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1dv607_workshop2_kr222if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
